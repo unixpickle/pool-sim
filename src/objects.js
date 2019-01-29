339930f1c4ca51ee9cfcaaf17f9d1e248a423fbe
Alex Nichol <unixpickle@gmail.com>
2019-01-29 10:47:37 -0500

    drafted barrier collisions

--- src/objects.js
+++ src/objects.js
@@ -40,6 +40,45 @@ class Ball extends Particle {
       ctx.restore();
     }
   }
+
+  collision(ball) {
+    if (ball === this) {
+      return null;
+    }
+    const dist = Math.sqrt(Math.pow(this.x - ball.x, 2) + Math.pow(this.y - ball.y, 2));
+    if (dist > this.radius + ball.radius) {
+      return null;
+    }
+    const normalX = ball.x - this.x;
+    const normalY = ball.y - this.y;
+    const invMag = 1 / Math.sqrt(Math.pow(normalX, 2) + Math.pow(normalY, 2));
+    return new Collision([invMag * normalX, invMag * normalY]);
+  }
+
+  containsPoint(x, y) {
+    const dist = Math.sqrt(Math.pow(x - this.x, 2) + Math.pow(y - this.y, 2));
+    return dist <= this.radius;
+  }
+
+  hitsLine(x1, y1, x2, y2) {
+    const vx = x2 - x1;
+    const vy = y2 - y1;
+
+    // Quadratic formula for ax^2 + bx + c.
+    const a = vx * vx + vy * vy;
+    const b = 2 * ((vx * x1 + vy * y1) + (vx * this.x + vy * this.y));
+    const c = (this.x * this.x + this.y * this.y) + (x1 * x1 + y1 * y1) -
+      (this.radius * this.radius) - 2 * (this.x * x1 + this.y * y1);
+
+    const sqrtBody = (b * b - 4 * a * c);
+    if (sqrtBody < 0) {
+      return false;
+    }
+
+    const t1 = (-b + Math.sqrt(sqrtBody)) / (2 * a);
+    const t2 = (-b - Math.sqrt(sqrtBody)) / (2 * a);
+    return (t1 >= 0 && t1 <= 1) || (t2 >= 0 && t2 <= 1);
+  }
 }
 
 class Collision {
@@ -59,16 +98,36 @@ class Barrier {
 }
 
 class RectBarrier extends Barrier {
-  constructor(x, y, width, height) {
+  constructor(x, y, width, height, normal) {
     super();
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    this.normal = normal;
   }
 
   draw(ctx) {
     ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  collision(ball) {
+    if (this.containsPoint(ball.x, ball.y) ||
+      ball.containsPoint(this.x, this.y) ||
+      ball.containsPoint(this.x + this.width, this.y) ||
+      ball.containsPoint(this.x, this.y + this.height) ||
+      ball.containsPoint(this.x + this.width, this.y + this.height) ||
+      ball.hitsLine(this.x, this.y, this.x + this.width, this.y) ||
+      ball.hitsLine(this.x, this.y, this.x, this.y + this.height) ||
+      ball.hitsLine(this.x + this.width, this.y, this.x + this.width, this.y + this.height) ||
+      ball.hitsLine(this.x, this.y + this.height, this.x + this.width, this.y + this.height)) {
+      return new Collision(this.normal.slice());
+    }
+    return null;
+  }
+
+  containsPoint(x, y) {
+    return (x >= this.x && x <= this.x + this.width && y >= this.y && y <= this.y + this.height);
   }
 }
 
@@ -79,6 +138,13 @@ class TriangleBarrier extends Barrier {
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+
+    const midX = (this.x1 + this.x2) / 2;
+    const midY = (this.y1 + this.y2) / 2;
+    const normalX = midX - this.x3();
+    const normalY = midY - this.y3();
+    const invMag = 1 / Math.sqrt(Math.pow(normalX, 2) + Math.pow(normalY, 2));
+    this.normal = [invMag * normalX, invMag * normalY];
   }
 
   x3() {
@@ -96,5 +162,23 @@ class TriangleBarrier extends Barrier {
     ctx.lineTo(this.x3(), this.y3());
     ctx.closePath();
     ctx.fill();
+  }
+
+  collision(ball) {
+    if (this.containsPoint(ball.x, ball.y) ||
+      ball.containsPoint(this.x1, this.y1) ||
+      ball.containsPoint(this.x2, this.y2) ||
+      ball.containsPoint(this.x3(), this.y3()) ||
+      ball.hitsLine(this.x1, this.y1, this.x2, y2) ||
+      ball.hitsLine(this.x2, this.y2, this.x3(), this.y3()) ||
+      ball.hitsLine(this.x3(), this.y3(), this.x1, this.y1)) {
+      return new Collision(this.normal.slice());
+    }
+    return null;
+  }
+
+  containsPoint(x, y) {
+    // TODO: this.
+    return false;
   }
 }
