8ad4a22cd2f70fb222561e26dd5ed114e7897c73
Alex Nichol <unixpickle@gmail.com>
2019-01-31 11:01:43 -0500

    generate training data for behavior cloning

---
+++ experiments/make_data.js
@@ -0,0 +1,28 @@
+const poolsim = require('../build/poolsim.js');
+
+const agent = new poolsim.SearchAgent(new poolsim.RandomAgent(), 100);
+
+function outputDatum(game, action) {
+  const liveBalls = game.table.liveBalls.map((b) => {
+    return { number: b.number, x: b.x, y: b.y };
+  });
+  const actionObj = { type: action.constructor.name };
+  Object.keys(action).forEach((k) => actionObj[k] = action[k]);
+  process.stdout.write(JSON.stringify({ live: liveBalls, action: actionObj }) + '\n');
+}
+
+function runGame() {
+  const game = new poolsim.Game();
+  while (game.winner() === null) {
+    while (game.actionType() !== null) {
+      const action = agent.pickAction(game);
+      outputDatum(game, action);
+      game.act(action);
+    }
+    game.stepFully();
+  }
+}
+
+while (true) {
+  runGame();
+}
