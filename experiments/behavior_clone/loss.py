0d214078edeedeb997af8487af4b24e31a44b864
Alex Nichol <unixpickle@gmail.com>
2019-02-03 14:10:32 -0500

    fix up loss functions

--- experiments/behavior_clone/loss.py
+++ experiments/behavior_clone/loss.py
@@ -2,10 +2,6 @@
 Loss functions for behavior cloning.
 """
 
-import math
-
-import numpy as np
-import torch
 import torch.nn.functional as F
 
 from discretize import ActionDiscretizer
@@ -31,22 +27,6 @@ def clone_loss(model_out, actions):
     return total / len(actions)
 
 
-def shoot_loss(shoot_vec, action):
-    actionX = math.cos(action['angle'])
-    actionY = math.sin(action['angle'])
-    action_vec = torch.from_numpy(
-        np.array([actionX, actionY], dtype=np.float32)).to(shoot_vec.device)
-    norm = torch.sqrt(torch.sum(torch.pow(shoot_vec[:2], 2)))
-    dot_loss = 1 - torch.sum(action_vec * shoot_vec[:2]) / norm
-    power_loss = torch.pow(shoot_vec[2] - action['power'], 2)
-    return dot_loss + power_loss
-
-
-def place_loss(place_vec, action):
-    action_vec = torch.from_numpy(np.array([action['x'], action['y']], dtype=np.float32))
-    return torch.sum(torch.pow(place_vec - action_vec, 2))
-
-
 def discrete_loss(logits, choice):
-    probs = F.log_softmax(logits)
+    probs = F.log_softmax(logits, dim=-1)
     return -probs[choice]
